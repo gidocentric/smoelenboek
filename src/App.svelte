10db936c668deb73eb5a68534302b15c4e33214d
Gido Aartsen, van <g.vanaartsen@asito.nl>
2021-02-14 15:52:33 +0100

    implement smoelenboek

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,70 @@
 <script>
-	export let name;
+	let people = [
+		{name: "test", age:"26", city:"test"}
+	];
+	let showEdit = false;
+
+	let newPerson = {
+		name: "",
+		age: "",
+		city: "",
+	};
+
+	function addPerson() {
+		people = [...people, newPerson];
+
+		showEdit = false;
+	}
+
+	function cancel() {
+		newPerson.name = "";
+		newPerson.age = "";
+		newPerson.city = "";
+		showEdit = false;
+	}
+
+	function show() {
+		showEdit = true;
+	}
+
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Smoelenboek</h1>
+	<div>
+		<p>Voeg iemand toe!</p>
+		{#if !showEdit}
+			<button on:click={show}>Toevoegen</button>
+		{/if}
+		{#if showEdit}
+			<div class="edit">
+				<label
+					><strong>Naam:</strong><br />
+					<input type="text" bind:value={newPerson.name} />
+				</label>
+				<label
+					><strong>Leeftijd:</strong><br />
+					<input type="text" bind:value={newPerson.age} /></label
+				>
+				<label
+					><strong>Woonplaats:</strong><br /><input
+						type="text"
+						bind:value={newPerson.city}
+					/></label
+				>
+				<button on:click={cancel}>Annuleer</button>
+				<button on:click={addPerson}>Toevoegen</button>
+			</div>
+		{/if}
+	</div>
+	<div>
+		<ul>
+			{#each people as person}
+				<li>{person.name} - {person.age} - {person.city}</li>
+			{/each}
+		</ul>
+	</div>
 </main>
 
 <style>
@@ -14,12 +74,20 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
+	.edit {
+		width: fit-content;
+		margin-left: auto;
+		margin-right: auto;
+		text-align: left;
+	}
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+	ul{
+		list-style:none;
 	}
 
 	@media (min-width: 640px) {
